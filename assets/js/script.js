f8d53381a6c792eaa644d5663e1bc03d7d08daa5
EduardoGomes18 <eduardo_gomes1318@outlook.com>
2019-11-21 23:41:27 -0300

    Add javascript for mobile buttom and css adjusts for responsive.

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    this.document.querySelector(".menu_mobile").addEventListener("click", function(){
+        if(document.querySelector(".menu nav ul").style.display == 'flex'){
+        (document.querySelector(".menu nav ul").style.display = 'none')
+        }else{
+            (document.querySelector(".menu nav ul").style.display = 'flex')
+        }
+    })
+}
